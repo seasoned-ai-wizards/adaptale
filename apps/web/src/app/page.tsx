d34648f4d83e098849a6756486ad916231f8f0a4
chris <chris@vrational.com>
2025-04-26 19:40:27 +0200

    Adapting main page style

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
-import {auth} from "~/server/auth";
-import {api, HydrateClient} from "~/trpc/server";
+import { auth } from "~/server/auth";
+import { api, HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello({text: "from tRPC"});
+  const hello = await api.post.hello({ text: "from tRPC" });
   const session = await auth();
 
   if (session?.user) {
@@ -12,25 +12,28 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <main
-        className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl text-center font-extrabold tracking-tight sm:text-[5rem]">
-            Story Teller
-          </h1>
+          <div className="flex flex-col items-center justify-center">
+            <img
+              src="/adaptable.png"
+              alt="Logo"
+              alt={"logo"}
+            />
+          </div>
+
           {
             // session &&
-            (<div className="flex flex-col items-center gap-4">
-                <Link
-                  className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-                  href="/realtime"
-                  target="_blank"
-                >
-                  <h3 className="text-2xl font-bold">Create presentation</h3>
-                </Link>
-
-              </div>
-            )}
+            <div className="flex flex-col items-center gap-4">
+              <Link
+                className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
+                href="/realtime"
+                target="_blank"
+              >
+                <h3 className="text-2xl font-bold">Create presentation</h3>
+              </Link>
+            </div>
+          }
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
               {hello ? hello.greeting : "Loading tRPC query..."}
