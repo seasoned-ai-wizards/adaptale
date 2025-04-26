1f7027ffe90107a69d440e3071e7f99be1a347db
chris <chris@vrational.com>
2025-04-26 19:40:39 +0200

    Adapting main page style

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -15,11 +15,7 @@ export default async function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <div className="flex flex-col items-center justify-center">
-            <img
-              src="/adaptable.png"
-              alt="Logo"
-              alt={"logo"}
-            />
+            <img src="/adaptable.png" alt="Logo" />
           </div>
 
           {
