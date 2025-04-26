a935aa035a5d30fe3b32bc27568c37cf1cdaa945
chris <chris@vrational.com>
2025-04-26 18:57:48 +0200

    Adapting main page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -20,13 +20,13 @@ export default async function Home() {
           </h1>
           {
             // session &&
-            (<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+            (<div className="flex flex-col items-center gap-4">
                 <Link
                   className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-                  href="/onboarding"
+                  href="/realtime"
                   target="_blank"
                 >
-                  <h3 className="text-2xl font-bold">Start onboarding</h3>
+                  <h3 className="text-2xl font-bold">Create presentation</h3>
                 </Link>
 
               </div>
